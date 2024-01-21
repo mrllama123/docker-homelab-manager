1337e549e58cf6bfe9cad33966b361031621bcae
mrllama123 <piesrule123@gmail.com>
2024-01-21 23:07:37 +1300

    basic setup for schedule

--- src/celery.py
+++ src/celery.py
@@ -2,6 +2,8 @@ import os
 
 import src.docker as docker
 from celery import Celery
+from sqlalchemy_celery_beat.schedulers import DatabaseScheduler
+from sqlalchemy_celery_beat.models import PeriodicTask, IntervalSchedule
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
@@ -9,6 +11,15 @@ celery.conf.result_backend = os.environ.get(
     "CELERY_RESULT_BACKEND",
     "db+sqlite://",
 )
+celery.conf.beat_dburi = os.environ.get(
+    "CELERY_BEAT_DBURI",
+    "db+sqlite://",
+)
+
+celery.conf.beat_scheduler = DatabaseScheduler
+celery.conf.beat_schema = None
+
+beat = celery.Beat(loglevel="debug")
 
 
 @celery.task(name="create_volume_backup")
@@ -19,3 +30,5 @@ def create_volume_backup(volume_name: str):
 @celery.task(name="restore_volume_task")
 def restore_volume_task(volume_name: str, filename: str):
     docker.restore_volume(volume_name, filename)
+
+
