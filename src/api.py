4e2c431e59b387ee482352075ad22eb561d2fb35
Kylos <piesrule123@gmail.com>
2024-01-31 22:01:50 +1300

    rename vars tweaked get backup schedule

--- src/api.py
+++ src/api.py
@@ -6,9 +6,10 @@ from apscheduler.jobstores.base import ConflictingIdError
 from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Session, select
 
-from src.apschedule import add_backup_job, add_restore_job, setup_scheduler
+from src.apschedule import add_backup_job, add_restore_job, get_backup_schedule, setup_scheduler
 from src.db import Backups, create_db_and_tables, engine
 from src.docker import get_volume, get_volumes, is_volume_attached
+from apscheduler.job import Job
 from src.models import (
     BackupSchedule,
     BackupVolume,
@@ -21,22 +22,21 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 
-SCHEDULE = None
+SCHEDULER = None
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    global SCHEDULE
+    global SCHEDULER
     create_db_and_tables()
-    SCHEDULE = setup_scheduler()
+    SCHEDULER = setup_scheduler()
     yield
-    SCHEDULE.shutdown(wait=False)
+    SCHEDULER.shutdown(wait=False)
 
 
 def get_session():
     with Session(engine) as session:
         yield session
-
 
 app = FastAPI(lifespan=lifespan)
 
@@ -95,7 +95,7 @@ def api_backup_volume(volume_name: str) -> BackupVolumeResponse:
         )
 
     task = add_backup_job(
-        SCHEDULE, f"backup-{volume_name}-{str(uuid.uuid4())}", volume_name
+        SCHEDULER, f"backup-{volume_name}-{str(uuid.uuid4())}", volume_name
     )
 
     return {"message": f"Backup of {volume_name} started", "task_id": task.id}
@@ -109,7 +109,7 @@ def api_restore_volume(
     backup_volume: BackupVolume,
 ) -> BackupVolumeResponse:
     task = add_restore_job(
-        SCHEDULE,
+        SCHEDULER,
         f"restore-{backup_volume.volume_name}-{str(uuid.uuid4())}",
         backup_volume.volume_name,
         backup_volume.backup_filename,
@@ -121,19 +121,15 @@ def api_restore_volume(
 
 @app.get("/volumes/backup/schedule/{schedule_name}", description="Get a backup schedule")
 def api_get_backup_schedule(schedule_name: str) -> BackupSchedule:
-    job = SCHEDULE.get_job(schedule_name)
-    if not job:
+    schedule = get_backup_schedule(schedule_name)
+    if not schedule:
         raise HTTPException(
             status_code=404,
             detail=f"Schedule job {schedule_name} does not exist",
         )
+    return schedule
 
-    return BackupSchedule(
-        job_id=job.id,
-        volume_name=job.args[0],
-        crontab=job.trigger.fields,
-        periodic=job.trigger.interval,
-    )
+
 
 @app.post("/volumes/backup/schedule", description="Create a backup schedule")
 async def api_create_backup_schedule(
@@ -147,7 +143,7 @@ async def api_create_backup_schedule(
 
     try:
         job = add_backup_job(
-            SCHEDULE,
+            SCHEDULER,
             schedule_body.schedule_name,
             schedule_body.volume_name,
             schedule_body.crontab,
--- src/apschedule.py
+++ src/apschedule.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import os
 
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
@@ -6,7 +7,8 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.interval import IntervalTrigger
 
 from src.docker import backup_volume, restore_volume
-from src.models import ScheduleCrontab, SchedulePeriodic
+from src.models import BackupSchedule, ScheduleCrontab, SchedulePeriodic, SchedulePeriod
+from apscheduler.job import Job
 
 APSCHEDULE_JOBSTORE_URL = os.environ.get(
     "APSCHEDULE_JOBSTORE_URL", "sqlite:///example.sqlite"
@@ -94,3 +96,52 @@ def add_restore_job(
         replace_existing=False,
         coalesce=True,
     )
+
+
+def get_backup_schedule(
+    scheduler: AsyncIOScheduler, schedule_name: str
+) -> BackupSchedule | None:
+    job = scheduler.get_job(schedule_name)
+    if job:
+        return map_job_to_backup_schedule(job)
+
+    return job
+
+
+def map_job_to_backup_schedule(job: Job):
+    if isinstance(job.trigger, CronTrigger):
+        return BackupSchedule(
+            job_id=job.id,
+            volume_name=job.args[0],
+            crontab=ScheduleCrontab(
+                minute=job.trigger.minute,
+                hour=job.trigger.hour,
+                day=job.trigger.day,
+                month=job.trigger.month,
+                day_of_week=job.trigger.day_of_week,
+            ),
+        )
+    elif isinstance(job.trigger, IntervalTrigger):
+        trigger = [
+            { "period":interval, "every":getattr(job.trigger, interval)}
+            for interval in SchedulePeriod
+            if getattr(job.trigger, interval) > 0
+        ]
+        if not trigger:
+            raise ValueError("trigger not properly configured")
+
+        if len(trigger) > 1:
+            raise ValueError("More than one trigger config found")
+
+        return BackupSchedule(
+            job_id=job.id,
+            volume_name=job.args[0],
+            periodic=SchedulePeriodic(
+                **trigger[0]
+            ),
+        )
+    else:
+        return BackupSchedule(
+            job_id=job.id,
+            volume_name=job.args[0],
+        )
