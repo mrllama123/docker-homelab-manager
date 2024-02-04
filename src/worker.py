f335411c728f91e23fe1b4e7dfb948502cf8a934
Kylos <piesrule123@gmail.com>
2024-02-04 21:54:41 +1300

    remove unused file

--- src/worker.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timezone
-from src.docker import backup_volume
-def backup_volume_task(volume_name):
-    dt_now = datetime.now(tz=timezone.utc)
-    backup_file = f"{volume_name}-{dt_now.isoformat()}.tar.gz"
-    backup_volume(volume_name)
-
