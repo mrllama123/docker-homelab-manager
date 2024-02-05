f7fa051af922d42f77fe1bc771c58111f5698d45
mrllama123 <piesrule123@gmail.com>
2024-02-05 22:23:17 +1300

    draft - restore task

--- src/apschedule/tasks.py
+++ src/apschedule/tasks.py
@@ -1,9 +1,9 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 from sqlmodel import Session
-from src.docker import backup_volume
+from src.docker import backup_volume, restore_volume
 from src.db import engine
-from src.models import Backups
+from src.models import Backups, RestoredBackups, ScheduleBackups
 import pytz
 import os
 
@@ -11,7 +11,7 @@ TZ = os.environ.get("TZ", "UTC")
 BACKUP_DIR = os.getenv("BACKUP_DIR")
 
 
-def task_create_backup(volume_name: str, job_id: str) -> None:
+def task_create_backup(volume_name: str, job_id: str, is_schedule=False) -> None:
     with Session(engine) as session:
         dt_now = datetime.now(tz=pytz.timezone(TZ))
         backup_file = f"{volume_name}-{dt_now.isoformat()}.tar.gz"
@@ -22,6 +22,24 @@ def task_create_backup(volume_name: str, job_id: str) -> None:
             backup_file_name=backup_file,
             backup_created=dt_now.isoformat(),
             backup_path=os.path.join(BACKUP_DIR, backup_file),
+            volume_name=volume_name,
+            success=True,
+        )
+
+        session.add(backup)
+        session.commit()
+
+
+def task_restore_backup(volume_name: str, backup_file: str, job_id: str) -> None:
+    with Session(engine) as session:
+        dt_now = datetime.now(tz=pytz.timezone(TZ))
+        restore_volume(volume_name, BACKUP_DIR, backup_file)
+
+        backup = RestoredBackups(
+            restore_id=job_id,
+            backup_file_name=backup_file,
+            restore_created=dt_now.isoformat(),
+            restore_path=os.path.join(BACKUP_DIR, backup_file),
             volume_name=volume_name,
             success=True,
         )
