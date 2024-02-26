006dae1ac5dd9c042e974aa80b900107c14247ac
mrllama123 <piesrule123@gmail.com>
2024-02-26 21:53:30 +1300

    basic support to create schedule and remove pop up schedule window

--- src/routes/html.py
+++ src/routes/html.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlmodel import Session
+from typing import Annotated
 
 from src.db import get_session
 from src.models import CreateBackupSchedule
@@ -10,11 +11,15 @@ from src.routes.impl.volumes import (
     api_backups,
     api_volumes,
     api_list_backup_schedules,
+    api_create_backup_schedule,
 )
+import logging
 
 router = APIRouter(tags=["html"])
 
 templates = Jinja2Templates(directory="src/templates")
+
+logger = logging.getLogger(__name__)
 
 
 @router.get("/", description="home page", response_class=HTMLResponse)
@@ -74,24 +79,46 @@ def backup_schedules(request: Request):
     )
 
 
-@router.post(
+@router.get(
     "/volumes/backup/schedule/{volume_name}",
     description="create backup schedule",
     response_class=HTMLResponse,
 )
-def create_backup_schedule(
-    request: Request, volume_name: str, schedule: CreateBackupSchedule | None = None
-):
-    if schedule:
-        # create schedule
-        pass
+def create_backup_schedule_form(request: Request, volume_name: str):
     return templates.TemplateResponse(
         request, "create_backup_schedule.html", {"volume_name": volume_name}
     )
 
 
-# @router.post(
-#     "/volumes/backup/schedule/{volume_name}",
-#     description="create backup schedule",
-#     response_class=HTMLResponse,
-# )
+@router.post(
+    "/volumes/backup/schedule/{volume_name}",
+    description="create backup schedule",
+    response_class=HTMLResponse,
+)
+async def create_backup_schedule(
+    request: Request,
+    volume_name: str,
+    schedule_name: Annotated[str, Form()],
+    seconds: Annotated[str, Form()],
+    minutes: Annotated[str, Form()],
+    hours: Annotated[str, Form()],
+    days: Annotated[str, Form()],
+    months: Annotated[str, Form()],
+    days_of_week: Annotated[str, Form()],
+):
+    
+    schedule = CreateBackupSchedule(
+        schedule_name=schedule_name,
+        volume_name=volume_name,
+        crontab={
+            "seconds": seconds,
+            "minutes": minutes,
+            "hours": hours,
+            "days": days,
+            "months": months,
+            "days_of_week": days_of_week,
+        },
+    )
+
+    await api_create_backup_schedule(schedule)
+    return ""
