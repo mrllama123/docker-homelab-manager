e0203b17188f9171531edcb53b716faf7f5341f6
mrllama123 <piesrule123@gmail.com>
2024-06-15 18:40:16 +1200

    enable cors middleware to handle app working with proxys etc

--- src/main.py
+++ src/main.py
@@ -6,10 +6,15 @@ from fastapi.staticfiles import StaticFiles
 
 from src.apschedule.schedule import setup_scheduler
 from src.routes import api, html
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 logger = logging.getLogger(__name__)
+CORS_ORIGINS = (
+    os.getenv("CORS_ORIGINS").split(",") if os.getenv("CORS_ORIGINS") else ["*"]
+)
 
-
+logger.info(f"CORS allow_origins: {CORS_ORIGINS}")
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     scheduler = setup_scheduler()
@@ -18,6 +23,13 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 app.include_router(api.router)
 app.include_router(html.router)
