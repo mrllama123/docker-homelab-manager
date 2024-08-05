d76e303c987aa55fca930dc370d5d61f1d11c67c
mrllama123 <piesrule123@gmail.com>
2024-08-05 21:04:53 +1200

    add validation test cases for creating backup source

--- tests/routes/api/test_create_sftp_backup_source.py
+++ tests/routes/api/test_create_sftp_backup_source.py
@@ -25,6 +25,7 @@ def test_create_sftp_backup_source_password(client, session):
     assert response_data.get("password") == "verysecretpassword"
     assert len(list(session.exec(select(SftpBackupSource)).all())) == 1
 
+
 def test_create_sftp_backup_source_ssh_key(client, session):
     response = client.post(
         "/api/volumes/backups/source",
@@ -44,7 +45,50 @@ def test_create_sftp_backup_source_ssh_key(client, session):
     assert response_data.get("port") == 22
     assert response_data.get("username") == "root"
     assert response_data.get("ssh_key_type") == SshKeyTypes.RSA
-    assert response_data.get("ssh_key") == "-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEA6NF8iallvQVp22WDkTkyrtvPruB2uZjYGT1+XWFYe9z\nRUtxr0Yl/e/H+B33IqU4ZinVTnQx2Kb+M3AX4+sO7m484zTbo7AAACTk+EGeCIb5O\nunaf6h8mJPIy/u8=\n-----END RSA PRIVATE KEY-----\n"
+    assert (
+        response_data.get("ssh_key")
+        == "-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEA6NF8iallvQVp22WDkTkyrtvPruB2uZjYGT1+XWFYe9z\nRUtxr0Yl/e/H+B33IqU4ZinVTnQx2Kb+M3AX4+sO7m484zTbo7AAACTk+EGeCIb5O\nunaf6h8mJPIy/u8=\n-----END RSA PRIVATE KEY-----\n"
+    )
     assert response_data.get("id")
     assert len(list(session.exec(select(SftpBackupSource)).all())) == 1
 
+
+def test_create_sftp_backup_source_invalid_ssh_key(client, session):
+    response = client.post(
+        "/api/volumes/backups/source",
+        json=dict(
+            name="test-sftp",
+            hostname="127.0.0.1",
+            port=22,
+            username="root",
+            ssh_key_type=SshKeyTypes.RSA,
+        ),
+    )
+    assert response.status_code == 422
+    response_data = response.json()
+    assert response_data["detail"][0]["type"] == "value_error"
+    assert (
+        response_data["detail"][0]["msg"]
+        == "Value error, either password or ssh key must be set"
+    )
+
+
+def test_create_sftp_backup_source_invalid_ssh_key_type(client, session):
+    response = client.post(
+        "/api/volumes/backups/source",
+        json=dict(
+            name="test-sftp",
+            hostname="127.0.0.1",
+            port=22,
+            username="root",
+            ssh_key="-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEA6NF8iallvQVp22WDkTkyrtvPruB2uZjYGT1+XWFYe9z\nRUtxr0Yl/e/H+B33IqU4ZinVTnQx2Kb+M3AX4+sO7m484zTbo7AAACTk+EGeCIb5O\nunaf6h8mJPIy/u8=\n-----END RSA PRIVATE KEY-----\n",
+        ),
+    )
+
+    assert response.status_code == 422
+    response_data = response.json()
+    assert response_data["detail"][0]["type"] == "value_error"
+    assert (
+        response_data["detail"][0]["msg"]
+        == "Value error, if setting ssh key both ssh key and ssh key type need to be selected"
+    )
