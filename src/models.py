0f0a562cd6083de9728a8c5b95ad252cb6cdd1fc
Bob Bruce <piesrule123@gmail.com>
2024-08-21 22:28:03 +1200

    skip pylint check

--- src/models.py
+++ src/models.py
@@ -61,10 +61,10 @@ class BackupSchedule(BaseModel):
 
 
 class BackUpStatus(Enum):
-    Created = "Created"
-    InProgress = "InProgress"
-    Processed = "Processed"
-    Errored = "Errored"
+    Created = "Created" # pylint: disable=invalid-name
+    InProgress = "InProgress" # pylint: disable=invalid-name
+    Processed = "Processed" # pylint: disable=invalid-name
+    Errored = "Errored" # pylint: disable=invalid-name
 
 
 class Backups(SQLModel, table=True):
